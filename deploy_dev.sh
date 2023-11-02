a27b57bf2b974f3d00234b58d658fa3f4b329b80
shinohara.yu <shino@bireki.jp>
2023-11-02 12:55:03 +0900

    refs #2

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,2 +1,2 @@
-rsync --delete -av -e "ssh -p 8022" ./ shino@bireki.shino07:/home/shino/discore-dealer-console/
+rsync --delete -av -e "ssh -p 8022" ./ shino@bireki.shino07:/home/shino/discore-manager-console/
 
--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,1 +1,1 @@
-aws s3 cp dest*  s3://discore-prod-dealer-console/  --recursive --acl public-read
+aws s3 cp dest*  s3://discore-prod-manager-console/  --recursive --acl public-read
